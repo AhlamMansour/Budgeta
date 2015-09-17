ea87513405483dc0d4600adfc928efcdce63b265
Ahlam Mansor <ahlam.mansor@galilsoftware.com>
2015-09-17 16:45:41 +0300

    add changes

--- src/test/java/com/budgeta/test/BudgetLineSettings/BudgetLineSettingsTest.java
+++ src/test/java/com/budgeta/test/BudgetLineSettings/BudgetLineSettingsTest.java
@@ -2,7 +2,7 @@ package com.budgeta.test.BudgetLineSettings;
 
 import org.testng.annotations.Test;
 
-import com.budgeta.pom.BudgetLineSetting;
+
 import com.budgeta.pom.SecondaryBoard;
 import com.budgeta.test.WrapperTest;
 
